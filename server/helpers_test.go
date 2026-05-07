7c75d8f3559a6c157f3c01c87e1c02a0a68fafdf
Joseph Gibis <Joeyg61703@gmail.com>
2026-05-07 14:00:12 -0400

    refactor: create helper test file

---
+++ server/helpers_test.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+)
+
+func setupTestServer(t *testing.T) (*Manager, string, func()) {
+	t.Helper()
+	manager := NewManager()
+	server := httptest.NewServer(http.HandlerFunc(manager.HandleWSUpgradeRequest))
+	wsURL := strings.Replace(server.URL, "http", "ws", 1)
+	cleanup := func() {
+		server.Close()
+	}
+
+	return manager, wsURL, cleanup
+}
+
+func pollEvent(t *testing.T, delay time.Duration, retries int, callback func() bool) bool {
+	t.Helper()
+	for range retries {
+		if callback() {
+			return true
+		}
+		time.Sleep(delay)
+	}
+	return false
+}
--- server/manager_test.go
+++ server/manager_test.go
@@ -2,9 +2,6 @@ package server
 
 import (
 	"context"
-	"net/http"
-	"net/http/httptest"
-	"strings"
 	"testing"
 	"time"
 
@@ -35,27 +32,4 @@ func TestManager_WSRequest(t *testing.T) {
 	if !sessionCountTestPassed {
 		t.Fatalf("Expected manager to have %v session(s), manager has %v", expectedSessionCount, actualSessionCount)
 	}
-}
-
-func setupTestServer(t *testing.T) (*Manager, string, func()) {
-	t.Helper()
-	manager := NewManager()
-	server := httptest.NewServer(http.HandlerFunc(manager.HandleWSUpgradeRequest))
-	wsURL := strings.Replace(server.URL, "http", "ws", 1)
-	cleanup := func() {
-		server.Close()
-	}
-
-	return manager, wsURL, cleanup
-}
-
-func pollEvent(t *testing.T, delay time.Duration, retries int, callback func() bool) bool {
-	t.Helper()
-	for range retries {
-		if callback() {
-			return true
-		}
-		time.Sleep(delay)
-	}
-	return false
 }
